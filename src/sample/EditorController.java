dcbfb83ea0ff0f9ccbe0e2a284bcd10af4241b72
snoe9 <sfstull@bsu.edu>
2018-04-24 20:33:23 -0400

    adds a photo holder

--- src/sample/EditorController.java
+++ src/sample/EditorController.java
@@ -1,34 +1,32 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 
 
 public class EditorController {
     ImageView myImageView;
+    @FXML AnchorPane editorPane;
+    private int counter = 0;
     public void btnLoadEventListener(ActionEvent actionEvent) {
 
             FileChooser fileChooser = new FileChooser();
+
 
             //Set extension filter
             FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
@@ -52,6 +50,19 @@ public class EditorController {
 
 
         private void addPhotoToFolder(){
+        }
+        private void addPhotos(){
+
+        TextArea test = new TextArea();
+        AnchorPane.setBottomAnchor(test, 10.0+counter);
+        editorPane.getChildren().add(test);
+
+
 
         }
+
+    public void testButton(ActionEvent actionEvent) {
+        addPhotos();
+        System.out.println("test1");
+    }
 }
